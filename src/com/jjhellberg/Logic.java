31494bf2f958ed88532c66c3b37af1757eaf0066
Jesper Hellberg <jj.hellberg@gmail.com>
2018-01-01 13:03:48 +0100

    first commit

---
+++ src/com/jjhellberg/Logic.java
@@ -0,0 +1,16 @@
+package com.jjhellberg;
+
+public class Logic {
+
+    private int[][] setUpMatrix(String input) {
+
+        String [] lines = input.split("\n");
+        return null;
+
+    }
+
+    public String getChecksum(String input) {
+        setUpMatrix(input);
+        return null;
+    }
+}
---
+++ test/com/jjhellberg/LogicTest.java
@@ -0,0 +1,20 @@
+package com.jjhellberg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicTest {
+    private static final String TESTINPUT = "5\t1\t9\t5\n" +
+            "7\t5\t3\n" +
+            "2\t4\t6\t8";
+
+
+    Logic logic = new Logic();
+
+
+    @Test
+    void testInputSplit() {
+
+    }
+}
